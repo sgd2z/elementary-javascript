084c7a3ebec98fb15309762d4e242ef0496eb332
sgd2z <s1dd@s1dd.com>
2019-06-15 12:31:11 -0400

    chapter 17 update

--- Chapter17/iAmLearning.js
+++ Chapter17/iAmLearning.js
@@ -2,6 +2,8 @@ let num = Math.random()
 num = Math.floor(Math.random() * 10)
 num = Math.floor(Math.random() * 11) + 5
 
+let date = new Date()
+let date = new Date ('April 1, 2019 14:30')
 
 alert('go')
 let startTime = Date.now()
