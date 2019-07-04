d27747076d06bbda6ffd99df2ca82ce2dd832e0f
sgd2z <s1dd@s1dd.com>
2019-07-04 15:37:51 -0400

    logical operators

--- Chapter07/iAmLearning.js
+++ Chapter07/iAmLearning.js
@@ -11,3 +11,29 @@ while (position < momsBoringShoppingList.length) {
 	position = position + 1
 }
 
+momsBoringShoppingList = ["eggs",
+	"milk", 
+	"yogurt", 
+	"things on sale",
+	"bread",
+	"butter",
+	"pizza",
+	"corn"
+]
+
+while (position < momsBoringShoppingList.length) {
+	let item = momsBoringShoppingList[position] //get the item at position
+	if (item.length < 5 || item.startsWith("b")) {
+		document.write('I buy ' + item)
+	} else {
+		document.write('Dad buys ' + item)
+	}
+	document.write('<br>')
+	position = position + 1
+}
+
+let aNumber = 4
+let anotherNumber = 9
+if (aNumber > 5 && anotherNumber < 10) {
+	document.write("Yesss!!")
+}
